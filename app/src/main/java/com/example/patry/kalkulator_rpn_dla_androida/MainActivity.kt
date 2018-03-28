d03fcabe1c54ad75a9ec7c69dff814944358a0b4
Szczukox <33254494+Szczukox@users.noreply.github.com>
2018-03-28 22:39:46 +0200

    Create project

---
+++ app/src/main/java/com/example/patry/kalkulator_rpn_dla_androida/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.patry.kalkulator_rpn_dla_androida
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
